0f7b852d859b0707d6e39361f70cc8ef440d8c71
Carolina Reedy <carinvid@gmail.com>
2022-03-14 21:22:01 -0600

    adding jobs folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
-import jobs from "../views/Jobs.vue";
+import jobs from "../Jobs/Jobs.vue";
 
 const routes = [
   {
