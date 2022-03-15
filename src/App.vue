21244cc5cb1804f171076fa841318ac5160bad1d
Carolina Reedy <carinvid@gmail.com>
2022-03-14 20:31:51 -0600

    adding files

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-size: bold;
+  color: aqua;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
